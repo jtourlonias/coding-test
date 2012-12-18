b4353ad0027adb4b3b85c9638abc2f17f2afbb36
jtourlonias <jtourlonias@gmail.com>
2012-12-18 09:59:19 -0500

    first commit

---
+++ maven-test/src/main/java/com/julien/codility/Demo.java
@@ -0,0 +1,60 @@
+package com.julien.codility;
+
+public class Demo {
+
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Codility demo test
+	 * A zero-indexed array A consisting of N integers is given. 
+	 * An equilibrium index of this array is any integer P such that 0 ≤ P < N 
+	 * and the sum of elements of lower indices is equal to the sum of elements of higher indices. 
+	 * A[0] + A[1] + ... + A[P−1] = A[P+1] + ... + A[N−2] + A[N−1].
+	 * Sum of zero elements is assumed to be equal to 0. This can happen if P = 0 or if P = N−1.
+	 * 
+	 * @param tab - array
+	 * @return any of its equilibrium indices. The function should return −1 if no equilibrium index exists.
+	 */
+	public int equi(int A[]) {
+		
+		long sumLeft = 0;
+		long sumRight = 0;
+		
+		// test if A is not empty
+		if(A.length == 0)
+			return -1;
+		
+		// calculate the global sum
+		for (int i = 0; i < A.length; i++) {
+			sumRight = sumRight + A[i];
+		}
+		
+		// test equi index on the right position, if yes return it
+		if (sumRight - A[0] == 0)
+			return 0;
+		
+		// test equi index on the left position, if yes return it
+		if (sumRight - A[A.length - 1] == 0)
+			return A.length - 1;
+		
+		// search for equi index in the array, start on the second position from the left
+		sumLeft = 0;
+		sumRight = sumRight - A[0];
+		
+		for (int i = 1; i < A.length - 1; i++) {
+			
+			sumRight = sumRight - A[i];
+			sumLeft = sumLeft + A[i-1];
+			
+			// test if i is an equi index, if yes return it
+			if(sumLeft == sumRight) {
+				return i;
+			}
+		}
+		
+		// if no equi index exist, return -1
+		return -1;
+	}
+}
---
+++ maven-test/src/test/java/com/julien/codility/TestDemo.java
@@ -0,0 +1,81 @@
+package com.julien.codility;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestDemo {
+
+	public TestDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Test
+	public void testDemoEmptyArray() {
+
+		int tab[] = {};
+		Demo demo = new Demo();
+
+		// test empty Array
+		assertEquals(-1, demo.equi(tab));
+	}
+
+	@Test
+	public void testDemoArrayWithoutIndexEqui() {
+
+		int tab[] = { -1, 0, 1 };
+		Demo demo = new Demo();
+
+		// test Array without index equi
+		assertEquals(-1, demo.equi(tab));
+	}
+
+	@Test
+	public void testDemoArrayWithIndexEquiBeforeLastPosition() {
+
+		int tab[] = { 1, 1, 1, 1, 1, 1, 5 };
+		Demo demo = new Demo();
+
+		// test Array with index equi before the last position
+		assertEquals(5, demo.equi(tab));
+	}
+
+	@Test
+	public void testDemoArrayWithIndexEquiSecondPosition() {
+
+		int tab[] = { 5, 1, 1, 1, 1, 1, 1 };
+		Demo demo = new Demo();
+
+		// test Array with index equi on second position
+		assertEquals(1, demo.equi(tab));
+	}
+
+	@Test
+	public void testDemoArrayWithIndexEquiFirstPosition() {
+
+		int tab[] = { 0, 1, 5, 2, -4, 3, -7 };
+		Demo demo = new Demo();
+
+		// test Array with index equi on first position
+		assertEquals(0, demo.equi(tab));
+	}
+
+	@Test
+	public void testDemoArrayWithIndexEquiLastPosition() {
+
+		int tab[] = { -7, 1, 5, 2, -4, 3, 0 };
+		Demo demo = new Demo();
+
+		// test Array with index equi on last position
+		assertEquals(6, demo.equi(tab));
+	}
+
+	@Test
+	public void testDemoArrayWithIndexEquiPositionThree() {
+
+		int tab[] = { -7, 1, 5, 8, -4, 3, 0 };
+		Demo demo = new Demo();
+
+		// test Array with index equi on position three
+		assertEquals(3, demo.equi(tab));
+	}
+}
